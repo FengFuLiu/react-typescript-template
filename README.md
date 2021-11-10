b6f82233eaf52157176c9424503d19467c79028e
刘佳辉 <503630147@qq.com>
2021-11-10 15:51:59 +0800

    feat: vscode插件开发

---
+++ README.md
@@ -0,0 +1,13 @@
+# react-typescript-template README
+
+### Feature
+- 快捷生成React-Typescript页面/组件模版；
+- 名称自动转换为组件命名规范；
+
+
+- Generate React-Typescript page/component template quickly;
+- Name converted to the component naming convention automatically;
+
+![](https://raw.githubusercontent.com/aaaaahua/blog/main/public/images/Untitled.gif)
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,83 @@
+import * as vscode from 'vscode';
+const fs = require('fs');
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+enum SNIPPETS_TYPE {
+  component = '组件',
+  page = '页面'
+}
+
+type Props = {
+  name: string;
+  path: string;
+  type: string
+};
+
+const line2camelCase = (name: string) =>
+  name.replace(/\-(\w)/g, (_all, letter) => letter.toUpperCase());
+
+const generateComponent = ({ name, path, type }: Props) => {
+  const templateName = line2camelCase(name).replace(/^\S/, s => s.toUpperCase());
+
+  try {
+    const content = SNIPPETS_TYPE.page === type ?
+      `import React from 'react';
+
+type Props = {};
+
+const ${templateName}: React.FC<Props> = () => {
+  return <></>;
+};
+
+export default ${templateName};
+`
+      :
+      `import React from 'react';
+
+type Props = {};
+
+const ${templateName}: React.FC<Props> = () => {
+  return <></>;
+};
+
+export default React.memo(${templateName});
+`
+      ;
+    fs.mkdirSync(`${path}`);
+    fs.writeFileSync(`${path}/index.tsx`, content);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export function activate(context: vscode.ExtensionContext) {
+
+  const createPageCommand = vscode.commands.registerCommand('extension.createPage', async function (param) {
+    const folderPath = param.fsPath;
+    const name = await vscode.window.showInputBox({
+      placeHolder: `Enter your page name`
+    });
+    if (name) {
+      const path = `${folderPath}/${name}`;
+      generateComponent({ name, path, type: SNIPPETS_TYPE.page });
+    } else {
+      vscode.window.showWarningMessage('Input cancel');
+    }
+  });
+
+
+  const createComponentCommand = vscode.commands.registerCommand('extension.createComponent', async function (param) {
+    const folderPath = param.fsPath;
+    const name = await vscode.window.showInputBox({
+      placeHolder: `Enter your Component name`
+    });
+    if (name) {
+      const path = `${folderPath}/${name}`;
+      generateComponent({ name, path, type: SNIPPETS_TYPE.component });
+    } else {
+      vscode.window.showWarningMessage('Input cancel');
+    }
+  });
+
+  context.subscriptions.concat([createPageCommand, createComponentCommand]);
+}
